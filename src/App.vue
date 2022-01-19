1cf8b2805adad754eaa451ce473b0aefa3ba9221
forbelieveme <haner.avella@protonmail.com>
2022-01-19 10:11:16 -0500

    Importa el componente footer en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <Header title="HW"></Header>
+  <Footer></Footer>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
