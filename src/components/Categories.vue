6c267c835022cad2be97231657a52c97284f0dd1
forbelieveme <haner.avella@protonmail.com>
2022-01-19 17:50:08 -0500

    Inserta botones de navegacion para las categorias

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,9 +1,72 @@
 <template>
-    <h1>2s</h1>
+  <div class="container">
+    <h1>Categoria</h1>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <button type="button" class="btn btn-outline-secondary">
+            Secondary
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Categories'
-}
+  name: "Categories",
+  props: {
+    categories: Array,
+  },
+};
 </script>
