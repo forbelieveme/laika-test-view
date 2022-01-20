20fa1f6fbaa4f2d5132b4db86b6ca8d554185746
forbelieveme <haner.avella@protonmail.com>
2022-01-19 19:03:18 -0500

    Crea el componente de categoria individual

---
+++ src/components/Categorie.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="categorie">
+    <a
+      :href="categorie.url"
+      class="btn btn-primary btn-lg"
+      tabindex="-1"
+      role="button"
+      aria-disabled="true"
+      >{{ categorie.title }}</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Categorie",
+  props: { categorie: Object },
+};
+</script>
