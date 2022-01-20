25c0c48585fc6b6dfe4955200de30c8a6f381320
forbelieveme <haner.avella@protonmail.com>
2022-01-19 21:20:34 -0500

    Importa e inserta el tag de SalesNav en el Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,20 @@
 <template>
   <InfoNav></InfoNav>
   <MainNav class="sticky-top"></MainNav>
+  <SalesNav></SalesNav>
 </template>
 
 <script>
 import MainNav from "./Header/MainNav.vue";
 import InfoNav from "./Header/InfoNav.vue";
+import SalesNav from "./Header/SalesNav.vue";
 
 export default {
   name: "Header",
   components: {
     MainNav,
     InfoNav,
+    SalesNav,
   },
 };
 </script>
