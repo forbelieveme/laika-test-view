17e4ba41b2b9bab4dec7699d5f6637586b1602b8
forbelieveme <haner.avella@protonmail.com>
2022-01-23 15:03:37 -0500

    importa todos los elementos de tipo Fas de fontawesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPhone);
+
+library.add(fas);
 
 // createApp(App).mount("#app");
 
