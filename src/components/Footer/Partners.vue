f73a1b7662dc9625dc1b839bf136343ba59fd033
forbelieveme <haner.avella@protonmail.com>
2022-01-23 15:45:17 -0500

    Crea el componente de partners para el footer

---
+++ src/components/Footer/Partners.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="footer-convenios" class="col-12 mt-5">
+    <div class="row">
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div class="col-auto">
+        <a href=""
+          ><img
+            src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.5/mercadopago/logo__large.png"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Partners",
+  props: {
+    parters: Array,
+  },
+};
+</script>
