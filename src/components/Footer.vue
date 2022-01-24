5dfd51522015cbf15e2adbef1344f14ab59dd74b
forbelieveme <haner.avella@protonmail.com>
2022-01-23 21:19:56 -0500

    Importa e inserta los subcomponentes de Brand e Info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,75 +1,9 @@
 <template>
-  <footer class="pt-2">
-    <div class="row">
-      <!-- *********************** -->
-      <!-- Hidden in small screens -->
-      <!-- *********************** -->
-      <div class="col-12 d-none d-sm-block">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Home</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Features</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">About</a>
-          </li>
-        </ul>
-      </div>
+  <footer class="pt-2 container-fluid mt-5">
+    <div class="row justify-content-center">
+      <Brand />
 
-      <div class="col-12 d-none d-sm-block">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Home</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Features</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">About</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-12 d-none d-sm-block">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Home</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Features</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
-          </li>
-          <li class="nav-item mb-2">
-            <a href="#" class="nav-link p-0 text-muted">About</a>
-          </li>
-        </ul>
-      </div>
-
-      <!-- *********************** -->
-      <!-- Hidden in small screens -->
-      <!-- *********************** -->
+      <Info />
       <!-- TODO -->
       <!-- Insertar un arreglo con la informacion  -->
       <!-- de los elementos del acordeon -->
@@ -77,7 +11,7 @@
       <Accordion />
       <Accordion />
 
-      <div class="col-12">
+      <div class="col-12 col-lg-2">
         <form>
           <h5>Subscribe to our newsletter</h5>
           <p>Monthly digest of whats new and exciting from us.</p>
@@ -97,20 +31,15 @@
       </div>
     </div>
 
-    <!-- *********************** -->
-    <!-- Partners logos -->
-    <!-- *********************** -->
-
     <Partners :partners="partners" />
-    <!-- *********************** -->
-    <!-- Partners logos -->
-    <!-- *********************** -->
   </footer>
 </template>
 
 <script>
 import Partners from "./Footer/Partners.vue";
 import Accordion from "./Footer/Accordion.vue";
+import Brand from "./Footer/Brand.vue";
+import Info from "./Footer/Info.vue";
 
 export default {
   name: "Footer",
@@ -121,6 +50,8 @@ export default {
   components: {
     Partners,
     Accordion,
+    Brand,
+    Info,
   },
 };
 </script>
