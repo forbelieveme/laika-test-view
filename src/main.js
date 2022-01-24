bea8ae6202bd4c53243c9fe51a5d634f0ab712a5
forbelieveme <haner.avella@protonmail.com>
2022-01-23 19:26:54 -0500

    Insala e importa la libreria de los iconos de brandring de fontawesome

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 
-library.add(fas);
+library.add(fas, fab);
 
 // createApp(App).mount("#app");
 
