de3c5a4cd0793a15c730722db782c6381d6a6759
forbelieveme <haner.avella@protonmail.com>
2022-01-23 21:52:59 -0500

    Ajusta el grupo de input del correo electronico para el footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,21 +11,39 @@
       <Accordion />
       <Accordion />
 
-      <div class="col-12 col-lg-2">
+      <div class="col-12 col-lg-2 mt-5 mt-lg-0">
         <form>
           <h5>Subscribe to our newsletter</h5>
           <p>Monthly digest of whats new and exciting from us.</p>
           <div class="d-flex w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden"
-              >Email address</label
+            <div
+              class="
+                input-group
+                col-12 col-md-12 col-lg-12
+                offset-xl-1
+                col-xl-7
+                p-0
+              "
             >
-            <input
-              id="newsletter1"
-              type="text"
-              class="form-control"
-              placeholder="Email address"
-            />
-            <button class="btn btn-primary" type="button">Subscribe</button>
+              <input
+                type="text"
+                class="autocomplete_search_style form-control border-end-0"
+                name="autocomplete_search"
+                id="autocomplete_search"
+                placeholder="¿Qué necesita tu mascota?"
+              />
+              <button
+                type="button"
+                class="btn border border-start-0"
+              >
+                <span
+                  class=""
+                  style="width: 30px; height: 25px"
+                >
+                  <font-awesome-icon icon="angle-right" />
+                </span>
+              </button>
+            </div>
           </div>
         </form>
       </div>
