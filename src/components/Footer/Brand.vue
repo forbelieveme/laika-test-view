775520026fbf598bbe7c272e7c57f049323bd528
forbelieveme <haner.avella@protonmail.com>
2022-01-23 21:20:46 -0500

    Crea y configura los subcomponentes de Brand e Info

---
+++ src/components/Footer/Brand.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="col-md-4 col-lg-2 text-center">
+    <span>
+      <img
+        class="ml-2 text-right img-fluid img_laika_footer lazy"
+        data-src="https://laika.com.co/assets/LAIKA.png  "
+        src="https://laika.com.co/assets/LAIKA.png  "
+        style=""
+      />
+      <p class="ml-0 ml-md-2 mt-2 p_img_laika_footer mb-0">Síguenos en:</p>
+      <p class="ml-0 ml-md-1">
+        <span class="filter-contrast-1">
+          <a
+            class="text_decoration_general"
+            href="https://www.youtube.com/watch?v=gchln4oix68"
+          >
+            <font-awesome-icon
+              :icon="['fab', 'facebook']"
+              size="2x"
+              style="color: #603e85"
+            />
+          </a>
+        </span>
+        <span class="filter-contrast-1">
+          <a
+            class="text_decoration_general"
+            href="https://www.instagram.com/laikamascotas/?hl=es-la"
+          >
+            <font-awesome-icon
+              :icon="['fab', 'youtube']"
+              size="2x"
+              style="color: #603e85"
+            />
+          </a>
+        </span>
+        <span class="filter-contrast-1">
+          <a
+            class="text_decoration_general"
+            href="https://es-la.facebook.com/LaikaUniverso/"
+          >
+            <font-awesome-icon
+              :icon="['fab', 'instagram']"
+              size="2x"
+              style="color: #603e85"
+            />
+          </a>
+        </span>
+      </p>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Brand",
+};
+</script>
---
+++ src/components/Footer/Info.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="col-lg-6 d-none d-lg-block">
+    <div class="row">
+      <div class="col-lg-4">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Home</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Features</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">About</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-4">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Home</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Features</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">About</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-4">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Home</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Features</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">Pricing</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">FAQs</a>
+          </li>
+          <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
