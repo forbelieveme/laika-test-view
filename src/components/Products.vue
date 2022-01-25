5fb9bdc450101e951cb068db92e4816d9f987152
forbelieveme <haner.avella@protonmail.com>
2022-01-24 21:25:05 -0500

    Importa el subcomponente de Producto

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,41 +1,30 @@
 <template>
-  <div class="row d-flex justify-content-center product_width">
-    <h3 class="pt-2">Productos</h3>
-    <div :key="product.id" v-for="product in products" class="col-auto">
-      <div class="card" style="width: 18rem">
-        <img
-          :src="product.filename"
-          class="card-img-top img_card_products"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+  <div class="container overflow-hidden">
+    <div class="row d-flex justify-content-center product_width">
+      <h3 class="pt-2">Productos</h3>
+      <div :key="product.id" v-for="product in products" class="col-auto">
+        <Product :product="product" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Product from "./Product.vue";
+
 export default {
   name: "Products",
+  components: {
+    Product,
+  },
   props: {
     products: Array,
   },
 };
-// <Categorie :categorie="categorie" class="col"></Categorie>
 </script>
 
 <style scoped>
-.img_card_products {
-  width: auto !important;
-  height: 190px;
-}
+
 
 .product_width {
   width: 2600px;
