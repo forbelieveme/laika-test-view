399cef7c2313af2ceb8eeefb76db77aa8eb6ac40
forbelieveme <haner.avella@protonmail.com>
2022-01-24 22:04:44 -0500

    Ubica el boton en la parte baja del card y alinea el texto del titulo a la izquierda

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="card" style="width: 18rem">
+  <div class="card" style="width: 17rem">
     <img
       :src="product.filename"
-      class="card-img-top img_card_products"
+      class="card-img-top img_card_products mb-3"
       alt="..."
     />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
+    <div class="card-body d-flex flex-column p-0">
+      <h5 class="card-title d-flex justify-content-start ms-3">Card title</h5>
+      <p class="card-text ms-3 text-start">
         Some quick example text to build on the card title and make up the bulk
         of the card's content.
       </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
+    <a href="#" class="btn btn-primary col-12 m-0">Go somewhere</a>
   </div>
 </template>
 
