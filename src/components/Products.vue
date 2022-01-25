40fb972c1072f3cef545b8b07e0a09086b395a75
forbelieveme <haner.avella@protonmail.com>
2022-01-24 20:03:11 -0500

    Genera dinamicamente los productos en tarjetas de bootstrap

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,6 +1,24 @@
 <template>
-  <h1>hola</h1>
-  <p></p>
+  <div class="row d-flex justify-content-center product_width">
+    <h3 class="pt-2">Productos</h3>
+    <div :key="product.id" v-for="product in products" class="col-auto">
+      <div class="card" style="width: 18rem">
+        <img
+          :src="product.filename"
+          class="card-img-top img_card_products"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +28,16 @@ export default {
     products: Array,
   },
 };
+// <Categorie :categorie="categorie" class="col"></Categorie>
 </script>
+
+<style scoped>
+.img_card_products {
+  width: auto !important;
+  height: 190px;
+}
+
+.product_width {
+  width: 2600px;
+}
+</style>
