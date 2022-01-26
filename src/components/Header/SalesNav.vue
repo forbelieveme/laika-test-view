6deae18762b6aef8e54c6427c22493a534e5c64f
forbelieveme <haner.avella@protonmail.com>
2022-01-25 23:09:52 -0500

    Inserta el menu de pantalla mayor a 1024

--- src/components/Header/SalesNav.vue
+++ src/components/Header/SalesNav.vue
@@ -11,10 +11,7 @@
     "
   >
     <a href="#" class="col-4 offset-2">
-      <div
-        class=""
-        style="cursor: pointer; background: #603e85; color: #fff"
-      >
+      <div class="" style="cursor: pointer; background: #603e85; color: #fff">
         <span class="ml-2" style="background: #603e85; color: #fff">
           Perro
         </span>
@@ -28,7 +25,7 @@
       </div>
     </a>
     <a href="#" class="col-4">
-      <div class=" ml-2" style="cursor: pointer">
+      <div class="ml-2" style="cursor: pointer">
         <span class="ml-2" style=""> Gato </span>
         <span class="text-left text-white mr-1 ml-1">
           <img
@@ -64,6 +61,132 @@
       </div>
     </div>
   </div>
+
+  <div class="navbar bg-white d-none d-xl-block">
+    <div class="container-fluid">
+      <div class="d-flex flex-row w-100 justify-content-between">
+        <div class="d-flex w-40 justify-content-center">
+          <div
+            id="menu_perro"
+            aria-haspopup="true"
+            aria-expanded="false"
+            class="mx-3 align-self-center"
+          >
+            <span class="text-left text-white">
+              <img
+                width="30"
+                src="https://laika.com.co/assets/home/dog_mc.svg "
+              />
+              <span id="active-dog" class="color_purple align-self-center">
+                Compra para perro
+              </span>
+              <img
+                src="https://laika.com.co/assets/punto_de_flecha_a_la_derecha_1.svg"
+                alt=""
+                class=""
+                style="height: 11px"
+              />
+            </span>
+          </div>
+          <div
+            id="menu_gato"
+            aria-haspopup="true"
+            aria-expanded="false"
+            class="mx-3 align-self-center"
+          >
+            <span class="text-left text-white">
+              <img
+                width="30"
+                src="https://laika.com.co/assets/home/cat_mc_s.svg"
+              />
+              <span id="active-cat" class="color_purple">
+                Compra para gato
+              </span>
+              <img
+                src="https://laika.com.co/assets/punto_de_flecha_a_la_derecha_1.svg"
+                alt=""
+                class=""
+                style="height: 11px"
+              />
+            </span>
+          </div>
+        </div>
+        <!-- *************** -->
+        <!-- *************** -->
+        <!-- *************** -->
+        <div class="d-flex justify-content-evenly w-40">
+          <div
+            id="menu_ofertas"
+            aria-haspopup="true"
+            aria-expanded="false"
+            class="align-self-center"
+          >
+            <span class="text-left text-white">
+              <span id="active-ofertas" class="color_purple"> Ofertas </span>
+              <img
+                src="https://laika.com.co/assets/punto_de_flecha_a_la_derecha_1.svg"
+                alt=""
+                class=""
+                style="height: 11px"
+              />
+            </span>
+          </div>
+          <div
+            id="menu_servicios"
+            aria-haspopup="true"
+            aria-expanded="false"
+            class="align-self-center"
+          >
+            <span class="text-left text-white">
+              <span id="active-servicios" class="color_purple">
+                Servicios
+              </span>
+              <img
+                src="https://laika.com.co/assets/punto_de_flecha_a_la_derecha_1.svg"
+                alt=""
+                class=""
+                style="height: 11px"
+              />
+            </span>
+          </div>
+          <div
+            aria-haspopup="true"
+            aria-expanded="false"
+            class="align-self-center"
+          >
+            <span class="text-left">
+              <a href="#">
+                <span id="active-servicios" class="color_purple text-muted">
+                  Blog
+                </span>
+              </a>
+            </span>
+          </div>
+        </div>
+        <!-- ************** -->
+        <!-- ************** -->
+        <!-- ************** -->
+        <div class="d-flex justify-content-end ms-auto me-5">
+          <div class="">
+            <a href="#" class="pe-auto">
+              <img
+                width="160"
+                src=" https://laika.com.co/assets/laika_vetcare/membership_logo.png "
+              />
+            </a>
+          </div>
+          <div class="">
+            <a href="#" class="pe-auto">
+              <img
+                width="160"
+                src=" https://laika.com.co/assets/laika_vetcare/membership_logo.png "
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -73,5 +196,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
