59cf6e4779990395f7828a5fab4a292a21dae8d1
forbelieveme <haner.avella@protonmail.com>
2022-01-27 10:10:05 -0500

    Ajusta las margenes de los elementos de categorias y productos

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,12 +1,14 @@
 <template>
-  <h3 class="pt-2 text-lg-start fw-bolder mx-2">Categorias</h3>
-  <div class="row d-flex justify-content-center">
-    <div
-      :key="categorie.id"
-      v-for="categorie in categories"
-      class="col-12 col-lg-3"
-    >
-      <Categorie :categorie="categorie" class="col" />
+  <div class="mx-5">
+    <h3 class="pt-2 text-lg-start fw-bolder mx-2">Categorias</h3>
+    <div class="row d-flex justify-content-center">
+      <div
+        :key="categorie.id"
+        v-for="categorie in categories"
+        class="col-12 col-lg-3"
+      >
+        <Categorie :categorie="categorie" class="col" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="container overflow-scroll no_scroll">
-    <div class="row d-flex justify-content-start product_width">
-      <h3 class="pt-2">Productos</h3>
-      <div :key="product.id" v-for="product in products" class="col-auto">
-        <Product :product="product" />
+  <div class="mx-5"> 
+    <h3 class="pt-2 text-lg-start fw-bolder mx-2">Productos</h3>
+    <div class="container overflow-scroll no_scroll">
+      <div class="row d-flex justify-content-start product_width">
+        <div :key="product.id" v-for="product in products" class="col-auto">
+          <Product :product="product" />
+        </div>
       </div>
     </div>
   </div>
@@ -30,12 +32,12 @@ export default {
 
 /* Hide scrollbar for Chrome, Safari and Opera */
 .no_scroll::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 /* Hide scrollbar for IE, Edge and Firefox */
 .no_scroll {
-  -ms-overflow-style: none;  /* IE and Edge */
-  scrollbar-width: none;  /* Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 </style>
